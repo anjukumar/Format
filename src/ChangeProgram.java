c34f29da3d08a9ed2c81277676f8114a8353cc93
anjukumar <anjukumar.ak1@gmail.com>
2016-09-02 14:35:44 -0400

    Program to input ADDRESS

--- src/ChangeProgram.java
+++ src/ChangeProgram.java
@@ -1,16 +1,27 @@
 
+import java.util.Scanner;
 public class ChangeProgram {
 
 		 public static void main(String[] args) {
-		 int x;
-		 float y, z;
+		 Scanner keyboard = new Scanner(System.in);
 		 
-		 x = 5;
-		 y = 9.5f;
-		 
-		 z = x * y;
-
-		 System.out.println("The product is " + z);
+		 String firstInitial,lastName, houseNumber,streetName,streetType,city;
+		 System.out.println("First Initial:");
+		 firstInitial = keyboard.next();
+		 System.out.println("lastName Initial:");
+		 lastName = keyboard.next();
+		 System.out.println("house number:");
+		 houseNumber = keyboard.next();
+		 System.out.println("street name:");
+		 streetName = keyboard.next();
+		 System.out.println("street type:");
+		 streetType = keyboard.next();
+		 System.out.println("city:");
+		 city = keyboard.next();
+		 System.out.print(firstInitial + " " + lastName + " " + houseNumber + " "); 
+		 System.out.println(streetName + " " + streetType + " " + city);
+		 keyboard.nextLine();
+		 keyboard.close();
 		 }
 		}
 
