c18d77fd71862e7263d6bf3a8feaf52cf89fcacf
anjukumar <anjukumar.ak1@gmail.com>
2016-09-02 15:55:20 -0400

    Special Char

---
+++ src/SpecialChar.java
@@ -0,0 +1,17 @@
+
+public class SpecialChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	    String message1, message2;
+	    
+	    message1 = "\\/\\/\\/\\/\\/\\r\\t\\b";
+	    message2 =  "\n\"; " ; 
+	    System.out.println(message1 + message2);
+	            
+	}
+
+	}
+
+
