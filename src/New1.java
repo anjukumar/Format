a6738a0b4a6e591e0874f8dbdc333f81cd15f689
anjukumar <anjukumar.ak1@gmail.com>
2016-09-02 13:43:34 -0400

    Class ignore

--- src/New1.java
+++ src/New1.java
@@ -1,10 +1,20 @@
 
 public class New1 {
 
-	public static void main(String[] args) {
-		System.out.println("My name is Anju ");
-		System.out.println("I am 24");
+	public static void main( String[] args ) { 
+        //declare variables here before we use them 
+		 	
+				int mynumber;
+				float myothernumber;
+				mynumber = 2;
+				myothernumber = 1.793f;
+				
+			 System.out.println(" My number " +mynumber);
+			 System.out.println(" My number " +myothernumber);
+			 
+			 }
+			
+	
+		}
 
-	}
 
-}
